f36fc32b6e229deb0613fb044d308241b88cf0a1
Vignika <20eg112132@anurag.edu.in>
2024-08-12 15:19:22 +0530

    redux-logger and redux devtool

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-// import CakeContainer from "./components/CakeContainer";
-// import HooksCakeContainer from "./components/HooksCakeContainer";
+import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 // import DocTitleOne from "./hooks/DocTitleOne";
 // import DocTitleTwo from "./hooks/DocTitleTwo";
-import CounterOne from "./hooks/CounterOne";
-import CounterTwo from "./hooks/CounterTwo";
+// import CounterOne from "./hooks/CounterOne";
+// import CounterTwo from "./hooks/CounterTwo";
 //npm i react-redux
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        {/* <CakeContainer />
-        <HooksCakeContainer /> */}
+        <CakeContainer />
+        <HooksCakeContainer />
         {/* <DocTitleOne/>
         <DocTitleTwo/> */}
 
-        <CounterOne/>
-        <CounterTwo/>
+        {/* <CounterOne/>
+        <CounterTwo/> */}
       </div>
     </Provider>
   );
